803382d4c7199dddfa86133e6cd142db05ff4768
pradeepkmaran <pradeepkm6122004@gmail.com>
2024-07-25 05:29:53 +0530

    GET /api/users/:id

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const PORT = 8000;
 const users = require("./MOCK_DATA.json");
 
 
+// HYBRID SERVER 
+
 app.get("/users", (req, res) => { // for browsers // SSR
     const html = `
     <ul>
@@ -15,6 +17,12 @@ app.get("/users", (req, res) => { // for browsers // SSR
 
 app.get('/api/users', (req, res) => { // for mobile apps or anything // CSR
     res.send(users);
-})
+});
+
+app.get('/api/users/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    res.json(user);
+});
 
 app.listen(PORT, ()=>console.log(`Listening to port: ${PORT}`));
