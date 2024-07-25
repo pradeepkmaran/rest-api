44932467687ed6e609e768e440bc3fca82470f96
pradeepkmaran <pradeepkm6122004@gmail.com>
2024-07-25 05:38:03 +0530

    route

--- index.js
+++ index.js
@@ -15,29 +15,30 @@ app.get("/users", (req, res) => { // for browsers // SSR
     return res.send(html);
 });
 
-app.get('/api/users', (req, res) => { // for mobile apps or anything // CSR
-    return res.send(users);
-});
 
-app.get('/api/users/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
-    return res.json(user);
-});
+app.route('/api/users')
+    .get((req, res) => { // for mobile apps or anything // CSR
+        return res.send(users);
+    })
+    .post('/api/users', (req, res) => {
+        // Create new user
+        return res.json({status: 'Pending'});
+    });
 
-app.post('/api/users', (req, res) => {
-    // Create new user
-    return res.json({status: 'Pending'});
-});
+app.route('/api/users/:id')
+    .get('/api/users/:id', (req, res) => {
+        const id = Number(req.params.id);
+        const user = users.find((user) => user.id === id);
+        return res.json(user);
+    })
+    .patch('/api/users/:id', (req, res) => {
+        // Update user with ID = id
+        return res.json({status: 'Pending'});
+    })
+    .delete('/api/users/:id', (req, res) => {
+        // Delete user with ID = id
+        return res.json({status: 'Pending'});
+    });
 
-app.patch('/api/users/:id', (req, res) => {
-    // Update user with ID = id
-    return res.json({status: 'Pending'});
-});
-
-app.delete('/api/users/:id', (req, res) => {
-    // Delete user with ID = id
-    return res.json({status: 'Pending'});
-});
 
 app.listen(PORT, ()=>console.log(`Listening to port: ${PORT}`));
