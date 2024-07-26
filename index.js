82ea603eab4c5b4916b5e6662e3b8773267fffa3
pradeepkmaran <pradeepkm6122004@gmail.com>
2024-07-26 08:09:51 +0530

    more middlewares

--- index.js
+++ index.js
@@ -5,8 +5,37 @@ const app = express();
 const PORT = 8000;
 
 
-// Middleware
+// Middleware 1
 app.use(express.urlencoded({extended: false}));
+
+// Middleware 2
+app.use((req, res, next) => {
+    fs.appendFile(
+        'log.txt', 
+        `MidWare1 : ${Date.now()} : ${req.ip}, ${req.path}\n`,
+        (err) => {
+            next();
+        }
+    );
+        
+});
+
+// Middleware 3
+app.use((req, res, next) => {
+    var change = 0; // change this to 1 if api call has to be made.
+    fs.appendFile(
+        'log.txt', 
+        (change == 1 ? `MidWare2 : ${Date.now()} : ${req.ip}, ${req.path}\n` : `MidWare2 : Abort Call`),
+        (err) => {
+            if(change) {
+                next();
+            } else {
+                return res.end("Abort Call"); 
+            }
+        } 
+    );  
+});
+
 
 // HYBRID SERVER 
 app.get("/users", (req, res) => { // for browsers // SSR
