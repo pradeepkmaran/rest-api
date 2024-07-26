e3315b7c34f4e16e782edc5a6a6d5820e82224f9
pradeepkmaran <pradeepkm6122004@gmail.com>
2024-07-27 05:21:32 +0530

    post request mongo db creation

--- index.js
+++ index.js
@@ -85,16 +85,20 @@ app.route('/api/users') // for mobile apps or anything // CSR
         res.setHeader('X-CreatedBy', `${req.ip}`); // header // explore in google to check inbuilt headers
         return res.send(users);
     })
-    .post((req, res) => {
+    .post(async (req, res) => {
         const body = req.body;
         if(!body || !body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title) {
             return res.status(400).json({msg : "All fields are required!"});
         }
-        const newUser = { id: users.length + 1, ...body};
-        users.push(newUser);
-        fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-            return res.status(201).json({status: "success", id: newUser.id});
-        })
+        const result = await User.create({
+            firstName: body.first_name,
+            lastName: body.last_name,
+            email: body.email,
+            gender: body.gender,
+            jobTitle: body.job_title,
+        });
+        console.log("user: ", result);
+        return res.status(201).json({msg: "success"});
     });
 
 app.route('/api/users/:id') // for mobile apps or anything // CSR
