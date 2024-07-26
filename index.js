d292f3589bfa4be9f893015de096890d890da1d1
pradeepkmaran <pradeepkm6122004@gmail.com>
2024-07-26 07:20:57 +0530

    middleware

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const users = require("./MOCK_DATA.json");
+const fs = require("fs");
 const app = express();
 const PORT = 8000;
-const users = require("./MOCK_DATA.json");
 
+
+// Middleware
+app.use(express.urlencoded({extended: false}));
 
 // HYBRID SERVER 
-
 app.get("/users", (req, res) => { // for browsers // SSR
     const html = `
     <ul>
@@ -16,16 +19,20 @@ app.get("/users", (req, res) => { // for browsers // SSR
 });
 
 
-app.route('/api/users')
-    .get((req, res) => { // for mobile apps or anything // CSR
+app.route('/api/users') // for mobile apps or anything // CSR
+    .get((req, res) => { 
         return res.send(users);
     })
     .post((req, res) => {
-        // Create new user
-        return res.json({status: 'Pending'});
+        const body = req.body;
+        const newUser = { id: users.length + 1, ...body};
+        users.push(newUser);
+        fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+            return res.json({status: "success", id: newUser.id});
+        })
     });
 
-app.route('/api/users/:id')
+app.route('/api/users/:id') // for mobile apps or anything // CSR
     .get((req, res) => {
         const id = Number(req.params.id);
         const user = users.find((user) => user.id === id);
