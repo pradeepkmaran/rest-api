ba71fe51598d99e6a5a78d832c75f86fa2a210cd
pradeepkmaran <pradeepkm6122004@gmail.com>
2024-07-26 12:48:31 +0530

    status codes

--- index.js
+++ index.js
@@ -55,10 +55,13 @@ app.route('/api/users') // for mobile apps or anything // CSR
     })
     .post((req, res) => {
         const body = req.body;
+        if(!body || !body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title) {
+            return res.status(400).json({msg : "All fields are required!"});
+        }
         const newUser = { id: users.length + 1, ...body};
         users.push(newUser);
         fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-            return res.json({status: "success", id: newUser.id});
+            return res.status(201).json({status: "success", id: newUser.id});
         })
     });
 
@@ -66,6 +69,7 @@ app.route('/api/users/:id') // for mobile apps or anything // CSR
     .get((req, res) => {
         const id = Number(req.params.id);
         const user = users.find((user) => user.id === id);
+        if(!user) return res.status(404).json({msg: "User not found."});
         return res.json(user);
     })
     .patch((req, res) => {
